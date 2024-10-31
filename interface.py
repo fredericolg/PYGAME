1d2067a15da31c6e2fbcd5660fc68dd84b10a0c9
fredericolg <fredericlg@al.insper.edu.br>
2024-10-31 13:35:52 -0300

    carregamento sem movimento - interface

--- interface.py
+++ interface.py
@@ -5,8 +5,11 @@ import pygame
 pygame.init()
 
 # ----- Gera tela principal
-window = pygame.display.set_mode((1080, 600))
+largura = 1200
+altura = 600
+window = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Penalty shoot out')
+#imagem = pygame.image.load("imagens\logo.webp")
 
 # ----- Inicia estruturas de dados
 game = True
@@ -20,7 +23,17 @@ while game:
             game = False
 
     # ----- Gera saídas
-    window.fill((0, 0, 100))  # Preenche com a cor branca
+    window.fill((0, 0, 80))  # Preenche com a cor branca
+    cor = (255, 255, 255)
+    vertices = [(200, 475), (200, 510), (1000, 510), (1000, 475)]
+    pygame.draw.polygon(window, cor, vertices, width=3)
+
+    i = 200
+    while i < 1000:
+        cor = (100, 255, 100)
+        vertices = [(203, 478), (203, 507), (997, 507), (997, 478)]
+        pygame.draw.polygon(window, cor, vertices)
+
 
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador

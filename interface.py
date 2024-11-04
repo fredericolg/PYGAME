47b8578f401a9ca12d4d58703219b97562f14655
fredericolg <fredericolg@al.insper.edu.br>
2024-11-03 23:01:23 -0300

    Botoes com animação

--- interface.py
+++ interface.py
@@ -11,10 +11,14 @@ largura = 1200
 altura = 600
 window = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Penalty shoot out')
-imagem_logo = pygame.image.load("imagens\logo_v2.png").convert_alpha()
+imagem_logo = pygame.image.load("imagens\logo2.png").convert_alpha()
 imagem_goal = pygame.image.load("imagens\GOAL!.png").convert_alpha()
 imagem_fundo = pygame.image.load("imagens\Tela.png").convert()
-
+imagem_play = pygame.image.load("imagens\imagem_play.png").convert_alpha()
+imagem_escrito = pygame.image.load("imagens\escrito.png").convert_alpha()
+imagem_play_mouse = pygame.image.load("imagens\play_cor.png").convert_alpha()
+imagem_config = pygame.image.load("imagens\Botao config2.png").convert_alpha()
+imagem_config_mouse = pygame.image.load("imagens\Botao config cor.png").convert_alpha()
 
 #========= Texto
 
@@ -52,16 +56,20 @@ while game:
         window.blit(imagem_fundo, (0, 0))
 
         if frames<=360:
-            cor = (255, 255, 255)
+            cor = (0, 0, 0)
             vertices = [(200, 450), (200, 480), (1000, 480), (1000, 450)]
 
             pygame.draw.polygon(window, cor, vertices, width=3)
+
+            cor = (255, 255, 255)
+            vertices = [(201, 451), (201, 479), (999, 479), (999, 451)]
+            pygame.draw.polygon(window, cor, vertices)
 
             cor = (0, 255, 0)
             vertices = [(203, 453), (203, 477), (203 + barra_carregamento, 477), (203 + barra_carregamento, 453)]
 
             pygame.draw.polygon(window, cor, vertices)
-            imagem_logo = pygame.transform.scale(imagem_logo, (600, 600))
+            imagem_logo = pygame.transform.scale(imagem_logo, (400, 400))
             imagem_logo_rect=imagem_logo.get_rect()
             imagem_logo_rect.center=((largura/2),(altura/2)-50)
             window.blit(imagem_logo, (imagem_logo_rect))
@@ -94,6 +102,50 @@ while game:
             # ----- Verifica consequências
             if event.type == pygame.QUIT:
                 game = False
+
+        cor = (255, 255, 255)
+        vertices = [(525, 330), (525, 395), (690, 395), (690, 330)]
+        retangulo_colisao = pygame.draw.polygon(window, cor, vertices, width = 1)
+
+        cor = (255, 255, 255)
+        vertices = [(550, 450), (550, 550), (670, 550), (670, 450)]
+        retangulo_colisao2 = pygame.draw.polygon(window, cor, vertices, width = 1)
+
+        imagem_escrito = pygame.transform.scale(imagem_escrito, (500, 500))
+        imagem_escrito_rect=imagem_escrito.get_rect()
+        imagem_escrito_rect.center=((largura/2),(altura/2)-150)
+        window.blit(imagem_escrito, (imagem_escrito_rect))
+
+
+        mouse_pos = pygame.mouse.get_pos()
+        if retangulo_colisao.collidepoint(mouse_pos):
+            imagem_play_mouse = pygame.transform.scale(imagem_play_mouse, (345, 485))
+            imagem_play_mouse_rect=imagem_play_mouse.get_rect()
+            imagem_play_mouse_rect.center=((largura/2)+11,(altura/2 + 80))
+            window.blit(imagem_play_mouse, (imagem_play_mouse_rect))
+        else:
+            imagem_play = pygame.transform.scale(imagem_play, (200, 100))
+            imagem_play_rect=imagem_play.get_rect()
+            imagem_play_rect.center=((largura/2)+10,(altura/2 + 65))
+            window.blit(imagem_play, (imagem_play_rect))
+
+        if retangulo_colisao2.collidepoint(mouse_pos):
+            imagem_config_mouse = pygame.transform.scale(imagem_config_mouse, (345, 335))
+            imagem_config_mouse_rect=imagem_config_mouse.get_rect()
+            imagem_config_mouse_rect.center=((largura/2)+10,(altura/2 + 200))
+            window.blit(imagem_config_mouse, (imagem_config_mouse_rect))
+        else:
+            imagem_config = pygame.transform.scale(imagem_config, (200, 185))
+            imagem_config_rect=imagem_config.get_rect()
+            imagem_config_rect.center=((largura/2)+10,(altura/2 + 200))
+            window.blit(imagem_config, (imagem_config_rect))
+        
+        print(mouse_pos)
+
+        
+            
+
+
 
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
